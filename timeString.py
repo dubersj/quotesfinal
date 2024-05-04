257eff804fe81b83e0fdd0ffffeab3e6bf5661ad
dubersj <54999089+dubersj@users.noreply.github.com>
2024-05-03 20:08:20 -0400

    timeString.py fixes single length minute

--- timeString.py
+++ timeString.py
@@ -2,7 +2,15 @@ from datetime import datetime
 
 def timeString():
     current_time = datetime.now()
+
+    # make minutes display correctly
+    if len(str(current_time.minute)) == 1:
+        minute = str(current_time.minute)
+        minute = "0" + minute
+    else:
+        minute = str(current_time.minute)
+
     saved_date = str(current_time.month) + '/' + str(current_time.day) + '/' + str(current_time.year)
-    saved_time = str(current_time.hour) + ':' + str(current_time.minute) + "UTC"
+    saved_time = str(current_time.hour) + ':' + minute + "UTC"
     saved_datetime_string = saved_date + ' ' + saved_time
     return saved_datetime_string
